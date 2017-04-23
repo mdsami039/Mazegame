ff5adb125cb994f3d4e5a837ed31b23eef223532
Samiuddin <mdsami039@gmai.com>
2017-04-23 17:01:38 +1000

    Mazegame Application
    Create the new character and attributes to character such as armour,
    strength, weapons, etc.  Adding a locations to a player to find a exit.
    
    If the player’s party encounters a hostile they will engage the party in
    combat
    • A player’s party can attack or flee
    • If a player’s party flees they do so through a random exit

---
+++ Control/ParsedInput.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mazegame.Control
+{
+    public class ParsedInput
+    {
+        private string command;
+        private ArrayList arguments;
+
+        public ParsedInput()
+        {
+            arguments = new ArrayList();
+            command = "";
+        }
+
+        public ParsedInput(string command, ArrayList arguments)
+        {
+            this.command = command;
+            this.arguments = arguments;
+        }
+
+        public string Command
+        {
+            get { return command; }
+            set { command = value; }
+        }
+
+        public ArrayList Arguments
+        {
+            get { return arguments; }
+            set { arguments = value; }
+        }
+    }
+}
---
+++ Control/Parser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mazegame.Control
+{
+    public class Parser
+    {
+        private ArrayList dropWords;
+        private ArrayList validCommands;
+
+        public Parser(ArrayList validCommands)
+        {
+            dropWords = new ArrayList(new string[]{"in","an","and",
+                "the","this", "to"});
+            this.validCommands = validCommands;
+        }
+
+        public ParsedInput Parse(String rawInput)
+        {
+            ParsedInput parsedInput = new ParsedInput();
+            String lowercaseInput = rawInput.ToLower();
+            ArrayList stringTokens = new ArrayList(rawInput.Split());
+
+            foreach (string token in stringTokens)
+            {
+
+                if (validCommands.Contains(token))
+                {
+                    parsedInput.Command = token;
+                }
+                else if (!dropWords.Contains(token))
+                    parsedInput.Arguments.Add(token);
+            }
+            return parsedInput;
+        }
+    }
+}
